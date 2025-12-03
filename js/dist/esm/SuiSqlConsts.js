e9c22acf4eca0193fc6ff1cb14ded03db0e5d287
J <jeka911@gmail.com>
2025-12-03 14:54:40 +0200

    fix low limits

--- js/dist/esm/SuiSqlConsts.js
+++ js/dist/esm/SuiSqlConsts.js
@@ -1,6 +1,6 @@
 const maxPureArgumentSize = 16 * 1024;
 const maxBinaryArgumentSize = Math.floor(3 * maxPureArgumentSize / 4) - 4;
-const maxMoveObjectSize = 15 * 1024;
+const maxMoveObjectSize = 150 * 1024;
 const packages = {
   local: "0x9010c9927792bca9df88323ea0fce0605d141a6d4d95e8e82697f0a810196144",
   testnet: "0x1d413bd519db4b3207c68766c33b362a384699cc7af34d97ad4dabdf223c223a",
