7b28ddf7b6e0bb93e61374e984586a96e579a7d3
J <jeka911@gmail.com>
2025-12-03 14:53:35 +0200

    fix low limits

--- js/src/SuiSqlConsts.ts
+++ js/src/SuiSqlConsts.ts
@@ -1,6 +1,6 @@
 const maxPureArgumentSize = 16 * 1024; // todo: add base64 conversion K
 const maxBinaryArgumentSize = Math.floor(3 * maxPureArgumentSize / 4) - 4;
-const maxMoveObjectSize = 15 * 1024; // keep it safer than Sui limit,
+const maxMoveObjectSize = 150 * 1024; // keep it safer than Sui limit,
 
 const packages:Object = {
         local: '0x9010c9927792bca9df88323ea0fce0605d141a6d4d95e8e82697f0a810196144',
